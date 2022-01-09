3282da5413d73d4f3234fcf9e82be85eba0393b4
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-01-09 11:03:58 +0530

    Create code.cpp

---
+++ Practical 15-File-handling-in-C++/Task-2-using-fstream/v2-write-into-the-file-using-fstream/code.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	fstream write;
+	string s;
+	cout<<"Enter the string : ";
+	getline(cin,s);
+	write.open("file.txt",ios::out);
+	write<<s;
+	write.close();
+	return 0;
+}
