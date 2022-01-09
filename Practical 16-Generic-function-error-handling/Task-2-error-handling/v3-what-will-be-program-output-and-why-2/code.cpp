76fe5f8ab482815dddc8ed0ca5894b3b0a039783
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-01-09 11:43:27 +0530

    Create code.cpp

---
+++ Practical 16-Generic-function-error-handling/Task-2-error-handling/v3-what-will-be-program-output-and-why-2/code.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+int main() 
+{
+    try 
+    { 
+        throw 'a';
+    }
+    catch(int x) 
+    {
+        cout<<"Caught ";
+    }
+    return 0; 
+}
