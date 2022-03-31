108b5c5c183f28c092574f3310f70035968e07b1
Hardik-Singh04 <hardikmanderwal04@gmail.com>
2022-03-31 21:58:53 +0530

    added code

---
+++ Practical - 12/Task-1-role-of-access-modifiers/v2-Accessing-base-class-elements-through-object/code.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class A
+{
+    private:
+        int a;  //inaccessible through child object
+    public:
+        int b;
+    protected:
+        int c;  //inaccessible through child object
+};
+class B:public A
+{
+    public:
+        void display()
+        {
+            cout<<"b="<<b<<endl;
+        }
+};
+int main()
+{
+    B obj;
+    obj.b=10;
+    obj.display();
+    return 0;
+}
