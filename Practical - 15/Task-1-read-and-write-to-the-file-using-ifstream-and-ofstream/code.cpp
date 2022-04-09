a02f84e553f8e7ee14da4c7551b9dad5b5c7c881
Hardik-Singh04 <hardikmanderwal04@gmail.com>
2022-04-09 21:26:57 +0530

    added code

---
+++ Practical - 15/Task-1-read-and-write-to-the-file-using-ifstream-and-ofstream/code.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	ofstream write;
+	ifstream read;
+	string s,s2;
+	cout<<"Enter the string : ";
+	getline(cin,s);
+	write.open("file.txt");
+	write<<s;
+	write.close();
+	read.open("file.txt");
+	getline(read,s2);
+	read.close();
+	cout<<"Content of file is : ";
+	cout<<s2<<endl;
+}
