4638cfcd1642faeb2717bfcc8a987c47483c016c
Hardik-Singh04 <hardikmanderwal04@gmail.com>
2023-01-01 06:29:37 +0530

    added code

---
+++ Practical - 7/Task-04 Restore_values/code.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	
+	cout << "Array is : " << endl;
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+			cout << arr[i][j] << " ";
+		cout << endl;
+	}	
+	
+	cout << "\nModified Array is : " << endl;
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+			cout << i+1 << " ";
+		cout << endl;
+	}		        
+	return 0;
+}
