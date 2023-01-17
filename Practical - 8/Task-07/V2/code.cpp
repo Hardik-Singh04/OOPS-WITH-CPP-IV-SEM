50b5ba1f23906cc71f0c915076a862306c72c9a3
Hardik-Singh04 <hardikmanderwal04@gmail.com>
2023-01-17 13:17:37 +0530

    added code

---
+++ Practical - 8/Task-07/V2/code.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+	int temp = b;
+	b = a;
+	a = temp;
+}
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	
+	swap(&a, &b);
+	
+	cout << "New value of a is: " << a <<endl;
+	cout << "New value of b is: " << b << endl;
+	
+	return 0;
+}
