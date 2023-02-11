048f7a876cac75dd5eb6bd39efa6bfe90eb891f8
Hardik-Singh04 <hardikmanderwal04@gmail.com>
2023-02-11 12:40:16 +0530

    added code

--- Practical -03 cin, cout and Header Files/TASK -01/iostream_dot_h.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream.h>
-int main()
-{
-	int i=10;
-	cout<<i;
-	return 0;
-}
--- Practical -03 cin, cout and Header Files/TASK-03/char_functioning.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	char a;
-	cin>>a;
-	cout<<a<<endl;
-	return 0;
-}
